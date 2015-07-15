b4dc212ae5b0716db2ba62caa81a5b525f821b06
Aaron <ufozrl@163.com>
2015-07-15 12:46:03 +0800

    1.修改readme文档 2.搭建入口文件的基础结构（包括关注回复和自定义菜单）

--- index.php
+++ index.php
@@ -8,12 +8,31 @@
 include "wechat.class.php";
 $options = array(
     'token'=>'vonvon', //填写你设定的key
-    'encodingaeskey'=>'encodingaeskey' //填写加密用的EncodingAESKey，如接口为明文模式可忽略
+    'encodingaeskey'=>'encodingaeskey', //填写加密用的EncodingAESKey，如接口为明文模式可忽略
+    'appid'=>'wx519f23f4a45e8c37',//填写高级调用功能的appid
+    'appsecret'=>'ea8f0b17b3a0882bf5fda7ed27758482' //填写高级调用功能的密钥
 );
 $weObj = new Wechat($options);
 $weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
 $type = $weObj->getRev()->getRevType();
 switch($type) {
+    case Wechat::EVENT_SUBSCRIBE:
+        $newsData = array(
+            "0"=>array(
+                'Title'=>'',
+                'Description'=>'',
+                'PicUrl'=>'',
+                'Url'=>''
+            ),
+            "1"=>array(
+                'Title'=>'',
+                'Description'=>'',
+                'PicUrl'=>'',
+                'Url'=>''
+            ),
+        );
+        $weObj->news($newsData)->reply();
+        break;
     case Wechat::MSGTYPE_TEXT:
         $weObj->text("hello,I'm Aaron!")->reply();
         exit;
@@ -25,3 +44,39 @@ switch($type) {
     default:
         $weObj->text("help info")->reply();
 }
+
+//获取菜单操作:
+$menu = $weObj->getMenu();
+//设置菜单
+$newmenu =  array(
+        "button"=>array(
+            0=>array(
+                'type'=>'click',
+                'name'=>'最新测试',
+                'sub_button'=>array(
+                    0=>array('type'=>'view','name'=>'','url'=>''),
+                    1=>array('type'=>'view','name'=>'','url'=>''),
+                    2=>array('type'=>'view','name'=>'','url'=>''),
+                    3=>array('type'=>'view','name'=>'','url'=>''),
+                    4=>array('type'=>'view','name'=>'','url'=>'')
+                )
+            ),
+            1=>array(
+                'type'=>'click',
+                'name'=>'人气测试',
+                'sub_button'=>array(
+                    0=>array('type'=>'view','name'=>'','url'=>''),
+                    1=>array('type'=>'view','name'=>'','url'=>''),
+                    2=>array('type'=>'view','name'=>'','url'=>''),
+                    3=>array('type'=>'view','name'=>'','url'=>''),
+                    4=>array('type'=>'view','name'=>'','url'=>'')
+                )
+            ),
+            2=>array(
+                'type'=>'view',
+                'name'=>'更多测试',
+                'url'=>'http://cn.vonvon.net'
+            )
+        )
+);
+$result = $weObj->createMenu($newmenu);
