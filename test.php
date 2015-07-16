7e38daad6d567027db955bae4ef3efc2e7fa5345
Aaron <ufozrl@163.com>
2015-07-16 10:12:45 +0800

    1.更新JSdemo

--- test.php
+++ test.php
@@ -25,18 +25,38 @@ switch($info['sex']) {
 }
 //$info['nickname'] = iconv('gbk2312','utf8',$info['nickname']);
 
+require_once "js/jssdk.php";
+$jssdk = new JSSDK("yourAppID", "yourAppSecret");
+$signPackage = $jssdk->GetSignPackage();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
 echo "Your nickname is ".$info['nickname']."<br />Your sex is ".$info['sex']."<br />Your city is ".$info['city']."<br />Your province is ".$info['province']."<br />Your country is ".$info['country']."<br />Now is ".date("Y-m-d H:i:s",time())."<br />";
 
 echo "<img src='".$info['headimgurl']."'>";
-print_r($weObj->getJsSign());
 ?>
-<html>
-<head>
-    <script type="text/javascript" src="jweixin-1.0.0.js"></script>
-    <script>
-        wx.config(
-            <?php echo json_encode($weObj->getJsSign());?>
-        );
+</body>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+    wx.config({
+        debug: true,
+        appId: '<?php echo $signPackage["appId"];?>',
+        timestamp: <?php echo $signPackage["timestamp"];?>,
+        nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+        signature: '<?php echo $signPackage["signature"];?>',
+        jsApiList: [
+            // 所有要调用的 API 都要加到这个列表中
+            'onMenuShareTimeline'
+        ]
+    });
+    wx.ready(function () {
+        // 在这里调用 API
         wx.onMenuShareTimeline({
             title: <?php echo $info['nickname'].', come on!';?>, // 分享标题
             link: 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx519f23f4a45e8c37&redirect_uri=http%3A%2F%2F139.129.117.49%2FvonvonWX%2Ftest.php&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1#wechat_redirect', // 分享链接
@@ -48,7 +68,6 @@ print_r($weObj->getJsSign());
                 // 用户取消分享后执行的回调函数
             }
         });
-    </script>
-</head>
-<body></body>
+    });
+</script>
 </html>
